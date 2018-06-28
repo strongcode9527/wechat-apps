ab51afc6367bd02cb473177d093a66da3901cd46
李壮 <1477869317@qq.com>
2018-06-28 13:35:18 +0800

    :sparkles:

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,35 +1,36 @@
-// pages/index/index.js
+// pages/detail/detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    meta: {},
-    topics: [],
-    name: 'strong',
+    topic: {},
+    content: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    const that = this
+    const data = getCurrentPages().pop(),
+          id = data.options.id,
+          that = this
+        
     wx.request({
-      url: 'http://www.mifanxing.com/api/article/topics/search?agg[size]=-1',
+      url: `http://www.mifanxing.com/api/article/topics/${id}`,
       data: {},
       method: 'get',
       success: function (res) {
         that.setData({
-          meta: res.data.meta,
-          topics: res.data.data,
+          topic: res.data.data,
         })
       },
       fail: function (res) {
@@ -42,17 +43,11 @@ Page({
     })
   },
 
-  handleClick: function(e) {
-    const id = e.currentTarget.id
-    id && wx.navigateTo({
-      url: `/pages/detail/detail?id=${id}`,
-    })
-  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
@@ -73,32 +68,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.log('refresh')
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    const that = this
-    wx.request({
-      url: `http://www.mifanxing.com/api/article/topics/search?agg[size]=-1&page[number]=${that.data.meta.number + 1}`,
-      data: {},
-      method: 'get',
-      success: function (res) {
-        that.setData({
-          meta: res.data.meta,
-          topics: that.data.topics.concat(res.data.data),
-        })
-      },
-      fail: function (res) {
-        wx.hideLoading()
-        fail()
-      },
-      complete: function (res) {
-
-      },
-    })
+  
   },
 
   /**
